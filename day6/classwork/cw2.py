3afc526cf6579d2faecb6176e26231fa14f1cf15
nika1777574 <nikaveliadze2@gmail.com>
2025-10-22 00:24:29 +0400

    `

--- day6/classwork/cw2.py
+++ day6/classwork/cw2.py
@@ -2,5 +2,8 @@ name=input("number: ")
 print(name)
 
 
-name=input("number: ")
-print(name)
+name1=input("number: ")
+name2=input("number: ")
+
+
+print=(name1+name2)
---
+++ day6/homework/hw1.py
@@ -0,0 +1,5 @@
+# input და output პროგრააამირებაში ნიშნავს ინფორმაციის შეყვანას და გამოტანას
+name1=input("number1: ")
+
+name2=input("number2: ") 
+
