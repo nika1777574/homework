a423e03bf634795de9f0df168ce58cf711627661
nika1777574 <nikaveliadze2@gmail.com>
2025-10-22 00:36:39 +0400

    1

---
+++ day6/homework/hw2.py
@@ -0,0 +1,8 @@
+
+sityva1 = input("შეიყვანე პირველი სიტყვა: ")
+sityva2 = input("შეიყვანე მეორე სიტყვა: ")
+
+
+sheerteba = sityva1 + sityva2
+
+print("შეერთებული სიტყვა:", sheerteba)
---
+++ day6/homework/hw3.py
@@ -0,0 +1,12 @@
+
+ricxvi1 = int(input("შეიყვანე პირველი რიცხვი: "))
+ricxvi2 = int(input("შეიყვანე მეორე რიცხვი: "))
+ricxvi3 = int(input("შეიყვანე მესამე რიცხვი: "))
+ricxvi4 = int(input("შეიყვანე მეოთხე რიცხვი: "))
+ricxvi5 = int(input("შეიყვანე მეხუთე რიცხვი: "))
+
+
+sashualo = (ricxvi1 + ricxvi2 + ricxvi3 + ricxvi4 + ricxvi5) / 5
+
+
+print("შეყვანილი რიცხვების საშუალო არითმეტიკული არის:", sashualo)
