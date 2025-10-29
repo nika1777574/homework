12cb6dd78a930e0ec35bb15a6532c6b79e5ca353
nika1777574 <nikaveliadze2@gmail.com>
2025-10-29 22:05:27 +0400

    1

---
+++ day8/homework/hw1.py
@@ -0,0 +1,63 @@
+# type() არის ფუნქცია, რომელიც ამოწმებს მისთვის გადაცემული მონაცემის ტიპს 
+
+# Data Conversion ნიშნავს მონაცემთა ტიპის გადაყვანას სხვა ტიპად.
+# მაგალითად, სტრინგის რიცხვად გადაყვანას, ან პირიქით.
+
+
+
+# text = "25"
+# print(type(text))  
+
+# num_from_text = int(text)
+# print(type(num_from_text))  
+
+
+
+
+# x = 10
+# print(type(x))  
+
+# nika_to_str = str(x)
+# print(type(nika_to_str))  
+
+
+# kkk = 5.75
+# print(type(kkk))  
+
+
+# kkk_to_int = int(kkk)
+# print(kkk_to_int)       
+# print(type(kkk_to_int)) 
+
+
+
+# is_sunny = True
+# print(type(is_sunny))  
+
+# bool_to_int = int(is_sunny)
+# print(bool_to_int)      
+# print(type(bool_to_int))  
+
+
+
+
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+num3 = int(input("შეიყვანე მესამე რიცხვი: "))
+
+
+print(type(num1))  
+print(type(num2))  
+print(type(num3))  
+
+
+num1_str = str(num1)
+num2_str = str(num2)
+num3_str = str(num3)
+
+print(type(num1_str))  
+print(type(num2_str)) 
+print(type(num3_str))  
+
+
+
