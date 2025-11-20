e25d29822f7538631c948df17c5b6b0f8782b30e
nika1777574 <nikaveliadze2@gmail.com>
2025-11-20 19:28:56 +0400

    1

--- day14/classwork/hw1.py
+++ day14/classwork/hw1.py
@@ -1,1 +1,11 @@
-for i in range():
+
+for i in range(100):
+    print(i)
+
+for i in range(101):
+    print(i)
+
+
+for i in range(5):
+    num = int(input("შეიყვანეთ რიცხვი: "))
+    print(num * num)
