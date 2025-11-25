146cfeeb73b9a34898fcc68c110cd5f94b7b81a0
nika1777574 <nikaveliadze2@gmail.com>
2025-11-26 00:28:31 +0400

    1

---
+++ day16/classwork/hw1.py
@@ -0,0 +1,23 @@
+number=100
+while number <=200:
+    print(number)
+    number+=1
+
+
+number = 0
+while number <= 20:
+    if number % 2 == 0:
+        print(number)
+    number += 1
+
+
+
+
+my_name = "nika"
+
+user_name = input("Enter your name: ")
+
+while user_name != my_name:
+    user_name = input("Enter your name: ")
+
+print("This is my name")
