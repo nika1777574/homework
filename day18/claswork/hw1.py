e18b6521c0eb044555f4588e91427e4cf5b7ac62
nika1777574 <nikaveliadze2@gmail.com>
2025-11-29 22:59:43 +0400

    1

--- day18/claswork/hw1.py
+++ day18/claswork/hw1.py
@@ -4,3 +4,24 @@ if num > 50:
     print(num * 5)
 else:
     print(num ** 2)
+
+
+
+password = input("შეიყვანეთ პაროლი: ")
+
+if password == "goa123":
+    print("Password is correct!")
+else:
+    print("Incorrect password!")
+
+
+
+
+num = int(input("შეიყვანეთ რიცხვი: "))
+
+niik = 0
+
+for i in range(1, num + 1):
+    niik += i
+
+print("ჯამი არის:", niik)
