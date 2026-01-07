eee3a4e105b277b44c641a2d91cfb906c0ceccaf
nika1777574 <nikaveliadze2@gmail.com>
2026-01-07 21:22:59 +0400

    1

---
+++ day25/classwork/hw1.py
@@ -0,0 +1,23 @@
+
+names = ["saba", "mari", "ana", "luka", "giorgi"]
+
+print( names)
+
+print( names[2])
+
+print( names[3])
+
+names[4] = "Nika"
+print( names)
+
+names[1], names[2] = names[2], names[1]
+print(names) 
+
+
+numbers = [5, 10, 15, 20, 25, 30, 35]
+
+print("სიის ელემენტთა რაოდენობაა:", len(numbers))
+
+name = input("შემოიყვანე შენი სახელი: ")
+
+print("შენს სახელში სიმბოლოთა რაოდენობაა:", len(name))
