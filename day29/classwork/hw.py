8b182c0476f216c264a018a4bc56fca42d72fd7d
nika1777574 <nikaveliadze2@gmail.com>
2026-01-21 20:04:22 +0400

    1

---
+++ day29/classwork/hw.py
@@ -0,0 +1,30 @@
+lst = [10, 20, 30, 40, 50, 60, 70]
+
+print(lst[0])
+
+print(lst[-1])
+
+print(lst[:3])
+
+print(lst[-2:])
+
+print(lst[1:-1])
+
+print(lst[::-1])
+
+print(lst[1])
+
+print(lst[::2])
+
+middle = len(lst) // 2
+print(lst[middle])
+
+print(lst[:len(lst)//2])
+
+
+
+
+
+
+
+
