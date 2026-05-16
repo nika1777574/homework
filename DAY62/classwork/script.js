9b0452b1064a4d4db4125d14ff05a68715081337
nika1777574 <nikaveliadze2@gmail.com>
2026-05-16 18:45:46 +0400

    1

---
+++ DAY62/classwork/script.js
@@ -0,0 +1,31 @@
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+
+// console არის ადგილი, სადაც პროგრამის შედეგები ჩანს
+// console.log() გამოიყენება ინფორმაციის გამოსატანად
+
+
+
+console.log("Hello" + " Nika");
+
+console.log("I am " + "student");
+
+let name = "Giorgi";
+console.log("Hello " + name);
+
+
+// Number რიცხვითი მონაცემები
+let num = 10;
+// String  ტექსტური მონაცემები
+let name = "Nika";
+// Boolean true ან false მნიშვნელობა
+let isAdmin = true;
+// Undefined ცვლადი რომელსაც მნიშვნელობა არ აქვ
+let age;
+// Null  ცარიელი მნიშვნელობა
+let user = null;
